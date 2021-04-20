9ea2ec7b70ab4370577b475ca13e9d3d2e22f44d
Janis Lebron <janislebron@gmail.com>
2021-04-20 23:59:23 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,13 @@
 # Create your haunted_houses migration here
+class CostumeStore < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :costume_inventory
+      t.integer :employees_count
+      t.string :in_business
+      t.integer :opening_time
+      t.integer :closing_time
+    end
+  end
+end
