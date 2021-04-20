2fcd21278a5ce218e4807270bedc3d8742ee0edf
Janis Lebron <janislebron@gmail.com>
2021-04-20 23:55:15 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,6 +12,9 @@ class CreateCostumes < ActiveRecord::Migration
   def change
     create_table :costumes do |t|
       t.string :name
+      t.integer :price
+      t.integer :size
+      t.string :imageurl
     end
   end
 end
