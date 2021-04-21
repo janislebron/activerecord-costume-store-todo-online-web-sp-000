aab8da50bb1b30dab98d6283909af5eb96196c3e
Janis Lebron <janislebron@gmail.com>
2021-04-21 00:18:21 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,15 +1,16 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+
   def change
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
       t.string :theme
-      t.integer :price
+      t.float :price
       t.boolean :family_friendly
-      t.integer :opening_date
-      t.integer :closing_date
-      t.string :long_description
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
     end
   end
 end
